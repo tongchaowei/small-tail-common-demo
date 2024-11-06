fe211fc689797d880d1cf4ce6f5a3a216b30c2f8
small tail <1974498278@qq.com>
2024-11-06 17:42:41 +0800

    新增：配置 Navie UI

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,10 @@ import {StCommonUIVue3Resolver} from "st-common-ui-vue3"
 import * as path from 'path'
 // 路由配置文件自动生成插件
 import Pages from 'vite-plugin-pages'
+// 自动引入 API 的插件
+import AutoImport from 'unplugin-auto-import/vite'
+// 按需自动加载 Naive UI 组件的解析器，用于帮助按需自动加载组件的插件自动按需加载 Navie UI 组件
+import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
 // 项目中 src 目录的绝对路径
 // __dirname 表示当前文件所在目录的绝对路径
@@ -17,11 +21,24 @@ const srcAbsolutePath: string = path.resolve(__dirname, './src')
 export default defineConfig({
   plugins: [
     vue(),
+    // 自动引入 API 的插件
+    AutoImport({
+      // 自动引入 Vue 和 Navie UI 中的 API
+      imports: [
+        'vue',
+        {
+          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar'],
+        },
+      ],
+      // 不要自动引入的 API
+      ignore: ['h']
+    }),
     // 配置按需自动加载组件的 Vite 插件
     Components({
       // 配置解析器，用于帮助按需自动加载组件的 Vite 插件解析并按需加载组件
       resolvers: [
-        StCommonUIVue3Resolver()
+        StCommonUIVue3Resolver(),
+        NaiveUiResolver(),
       ],
     }),
     // 路由配置文件自动生成插件
